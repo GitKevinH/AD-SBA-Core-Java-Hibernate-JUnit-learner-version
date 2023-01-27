c3259f37a007410a033a4c27efc78ec98d746b4e
kxh1-admin <kevin.hoang199@gmail.com>
2023-01-27 13:54:50 -0500

    Did not save changes to student class, saving and comitting to now

--- src/main/java/sba/sms/models/Student.java
+++ src/main/java/sba/sms/models/Student.java
@@ -1,5 +1,72 @@
 package sba.sms.models;
 
-public class Student {
+import java.util.List;
+import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
+@ToString
+
+@Entity
+@Table(name = "student")
+public class Student {
+	// fields
+	@Id
+	private String email;
+		@NonNull
+	private String name;
+	private String password;
+	
+	@ToString.Exclude
+	private List<Course> courses;
+	
+
+	// constructors
+
+
+	
+	
+	// tostring and hashcode and equals
+	
+	
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(courses, email, name, password);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		return Objects.equals(courses, other.courses) && Objects.equals(email, other.email)
+				&& Objects.equals(name, other.name) && Objects.equals(password, other.password);
+	}
+
+
+
+	
+	
+	
 }
