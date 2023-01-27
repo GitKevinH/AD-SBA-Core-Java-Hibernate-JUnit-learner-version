701978804f80677300f17bc994f2e3071442793b
kxh1-admin <kevin.hoang199@gmail.com>
2023-01-27 13:51:45 -0500

    Worked in requirements for Models

--- src/main/java/sba/sms/models/Course.java
+++ src/main/java/sba/sms/models/Course.java
@@ -1,5 +1,53 @@
 package sba.sms.models;
 
+import java.util.List;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
+@ToString
+
+@Entity
+@Table(name = "course")
 public class Course {
+	@Id
+	private int id;
+		@NonNull
+	private String name;
+	private String instructor;
+	
+	@ToString.Exclude
+	private List<Student> students;
+	
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, instructor, name, students);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Course other = (Course) obj;
+		return id == other.id && Objects.equals(instructor, other.instructor) && Objects.equals(name, other.name)
+				&& Objects.equals(students, other.students);
+	}
 
 }
